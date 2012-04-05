553bd7b69556613e8e169992c84fa85c03bd7031
Sreenu <sreenivasula@digitalgreen.org>
2012-04-06 01:10:18 +0530

    --removing code

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
-from betting.views import start, placebets, userhome, home
+from betting.views import start, placebets, home
 
 handler500 = 'djangotoolbox.errorviews.server_error'
 
@@ -9,8 +9,7 @@ urlpatterns = patterns('',
     ('^channel/$', direct_to_template, {'template': 'channel.html'}),
     ('^start/$', start),
     ('^home/$', home),
-	('^placebets/$', placebets),
-	('^userhome/$', userhome),
+	('^placebets/$', placebets),	
 #    ('^$', start),
 #    (r'^facebook/', include('django_facebook.urls')),
     ('^$', direct_to_template, {'template': 'home.html', 'extra_context': {'app_id':'direct_to_template'}}),
